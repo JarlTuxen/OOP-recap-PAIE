4f6b15239436b750850405317fa54ed220e250b0
jarltuxen <jarl.tuxen@gmail.com>
2021-02-03 10:21:20 +0100

    update Main

---
+++ src/dk/kea/Dat20v2/Main.java
@@ -0,0 +1,13 @@
+package dk.kea.Dat20v2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Initialize Machine and turn On and off
+
+        // add radio and printer
+
+        // polymorfi - print med instanceof
+
+    }
+}
